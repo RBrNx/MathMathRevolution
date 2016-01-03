5952abc5d7191a04c598571a803162574873ac48
RBrNx277 <b00231929@studentmail.uws.ac.uk>
2016-01-03 23:57:00 +0000

    Test

---
+++ Assets/Scene/Scripts/PressurePadScript.js
@@ -0,0 +1,44 @@
+﻿var PressurePad : GameObject;
+
+function OnTriggerEnter(Collision){
+	if(PressurePad.name == "PressurePadRight"){
+	PressurePad.GetComponent.<Animation>().Play("PressurePadDown-R");
+	}
+	if(PressurePad.name == "PressurePadLeft"){
+	PressurePad.GetComponent.<Animation>().Play("PressurePadDown-L");
+	}
+	if(PressurePad.name == "PressurePadFront"){
+	PressurePad.GetComponent.<Animation>().Play("PressurePadDown-F");
+	}
+	if(PressurePad.name == "PressurePadBack"){
+	PressurePad.GetComponent.<Animation>().Play("PressurePadDown-B");
+	}
+}
+
+function OnTriggerExit(Collision){
+	if(PressurePad.name == "PressurePadRight"){
+	PressurePad.GetComponent.<Animation>().Play("PressurePadUp-R");
+	}
+	if(PressurePad.name == "PressurePadLeft"){
+	PressurePad.GetComponent.<Animation>().Play("PressurePadUp-L");
+	}
+	if(PressurePad.name == "PressurePadFront"){
+	PressurePad.GetComponent.<Animation>().Play("PressurePadUp-F");
+	}
+	if(PressurePad.name == "PressurePadBack"){
+	PressurePad.GetComponent.<Animation>().Play("PressurePadUp-B");
+	}
+}
+
+/*function onTriggerExit(Other : Collider){
+	//if(Other.gameObject.tag == "PressurePad"){
+		Animator.GetComponent.<Animation>().Play("PressurePadUp");
+		Debug.Log("Up");
+	//}
+}*/
+
+/*var Animator : GameObject; //Assign this in the editor character with animation
+     
+    function OnTriggerEnter(){
+        Animator.GetComponent.<Animation>().Play("Animination"); // name the animation to be played
+    }*/
---
+++ Assets/Scene/Scripts/ProjectorScreen.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ProjectorScreen : MonoBehaviour {
+
+    Dictionary<string, List<Question>> questions;
+    public TextAsset questionsFile;
+    public int chosenTopic;
+    int questionNumber = 0;
+    GameObject gObj;
+    TextMesh question;
+    TextMesh answerOne;
+    TextMesh answerTwo;
+    TextMesh answerThree;
+    TextMesh answerFour;
+
+    // Use this for initialization
+    void Start () {
+		questions = Question.loadQuestions(questionsFile.text);
+        gObj = GameObject.Find("Question");
+        question = gObj.GetComponent<TextMesh>();
+
+        gObj = GameObject.Find("Answer 1");
+        answerOne = gObj.GetComponent<TextMesh>();
+
+        gObj = GameObject.Find("Answer 2");
+        answerTwo = gObj.GetComponent<TextMesh>();
+
+        gObj = GameObject.Find("Answer 3");
+        answerThree = gObj.GetComponent<TextMesh>();
+
+        gObj = GameObject.Find("Answer 4");
+        answerFour = gObj.GetComponent<TextMesh>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
