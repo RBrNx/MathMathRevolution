41302050ebf33787aea6e0abeb8d9de958e44405
RBrNx277 <b00231929@studentmail.uws.ac.uk>
2016-01-04 11:56:04 +0000

    Bug with Projector Screen at the moment, after adding code to display questions from XML

--- Assets/Scene/Scripts/ProjectorScreen.cs
+++ Assets/Scene/Scripts/ProjectorScreen.cs
@@ -6,14 +6,12 @@ public class ProjectorScreen : MonoBehaviour {
 
     Dictionary<string, List<Question>> questions;
     public TextAsset questionsFile;
-    public int chosenTopic;
+    string chosenTopic = "Topic 1 - Easy";
     int questionNumber = 0;
+    bool questionChange = true;
     GameObject gObj;
     TextMesh question;
-    TextMesh answerOne;
-    TextMesh answerTwo;
-    TextMesh answerThree;
-    TextMesh answerFour;
+    TextMesh[] answers;
 
     // Use this for initialization
     void Start () {
@@ -22,20 +20,53 @@ public class ProjectorScreen : MonoBehaviour {
         question = gObj.GetComponent<TextMesh>();
 
         gObj = GameObject.Find("Answer 1");
-        answerOne = gObj.GetComponent<TextMesh>();
+        answers[0] = gObj.GetComponent<TextMesh>();
 
         gObj = GameObject.Find("Answer 2");
-        answerTwo = gObj.GetComponent<TextMesh>();
+        answers[1] = gObj.GetComponent<TextMesh>();
 
         gObj = GameObject.Find("Answer 3");
-        answerThree = gObj.GetComponent<TextMesh>();
+        answers[2] = gObj.GetComponent<TextMesh>();
 
         gObj = GameObject.Find("Answer 4");
-        answerFour = gObj.GetComponent<TextMesh>();
+        answers[3] = gObj.GetComponent<TextMesh>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-	    
-	}
+
+        if (Input.GetKeyUp("space"))
+        {
+            questionNumber++;
+            questionChange = true;
+        }
+
+        if (questionChange)
+        {
+            int correctCount = questions[chosenTopic][questionNumber].correctAnswers.Count;
+            int incorrectCount = 4 - correctCount;
+            question.text = questions[chosenTopic][questionNumber].question;
+            int correctFilled = 0, incorrectFilled = 0;
+
+            while (correctFilled != correctCount && incorrectFilled != incorrectCount)
+            {
+                int random = Random.Range(0, 3);
+                if(answers[random] == null)
+                {
+                    if(correctFilled != correctCount)
+                    {
+                        answers[random].text = questions[chosenTopic][questionNumber].correctAnswers[correctFilled];
+                        correctFilled++;
+                    }
+                    else if (incorrectFilled != incorrectCount)
+                    {
+                        answers[random].text = questions[chosenTopic][questionNumber].correctAnswers[incorrectFilled];
+                        incorrectFilled++;
+                    }
+                }
+            }
+
+            questionChange = false;
+        }
+    }
 }
