4628d8448e6645d3639ae387bc5f210e2ed5c2aa
RBrNx277 <b00231929@studentmail.uws.ac.uk>
2016-01-04 21:32:23 +0000

    Pressure Pad Logic is now working

---
+++ Assets/Scene/Scripts/AskQuestion.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AskQuestion : MonoBehaviour {
+
+    Dictionary<string, List<Question>> questions;
+    public TextAsset questionsFile;
+    string chosenTopic = "Topic 1 - Easy";
+    int questionNumber = 0;
+    bool questionChange = true;
+    GameObject gObj;
+    TextMesh question;
+    TextMesh[] answers;
+	bool[] answerAdded = { false, false, false, false };
+    int correctCount;
+    int incorrectCount;
+
+    int pressurePadPressed;
+    bool pressurePadReleased = true;
+    int correctAnswers = 0;
+
+    int answeredCorrectly = 0;
+    int questionsAsked = 0;
+    int padCount = 0;
+
+    // Use this for initialization
+    void Start () {
+		answers = new TextMesh[4];
+		questions = Question.loadQuestions(questionsFile.text);
+
+        gObj = GameObject.Find("Question");
+        question = gObj.GetComponent<TextMesh>();
+
+        gObj = GameObject.Find("Answer 1");
+        answers[0] = gObj.GetComponent<TextMesh>();
+
+        gObj = GameObject.Find("Answer 2");
+        answers[1] = gObj.GetComponent<TextMesh>();
+
+        gObj = GameObject.Find("Answer 3");
+        answers[2] = gObj.GetComponent<TextMesh>();
+
+        gObj = GameObject.Find("Answer 4");
+        answers[3] = gObj.GetComponent<TextMesh>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp("space"))
+        {
+            questionNumber++;
+			ChangeQuestion();
+        }
+
+        CheckQuestions();
+        CheckAnswers();
+    }
+
+	public void ChangeQuestion()
+	{
+		questionChange = true;
+		answerAdded[0] = false;
+		answerAdded[1] = false;
+		answerAdded[2] = false;
+		answerAdded[3] = false;
+	}
+
+    public void setPressurePad(int padID)
+    {
+        pressurePadPressed = padID;
+    }
+
+    public void setPressurePadReleased(bool padRelease)
+    {
+        pressurePadReleased = padRelease;
+    }
+
+    void CheckQuestions()
+    {
+        if (questionChange)
+        {
+            padCount = 0;
+            correctAnswers = 0;
+            
+            correctCount = questions[chosenTopic][questionNumber].correctAnswers.Count;
+            incorrectCount = 4 - correctCount;
+            question.text = questions[chosenTopic][questionNumber].question;
+            int correctFilled = 0, incorrectFilled = 0;
+
+            while (correctFilled != correctCount || incorrectFilled != incorrectCount)
+            {
+                int random = Random.Range(0, 4);
+                if (!answerAdded[random])
+                {
+                    if (correctFilled != correctCount)
+                    {
+                        answers[random].text = questions[chosenTopic][questionNumber].correctAnswers[correctFilled];
+                        correctFilled++;
+                        answerAdded[random] = true;
+                    }
+                    else if (incorrectFilled != incorrectCount)
+                    {
+                        answers[random].text = questions[chosenTopic][questionNumber].incorrectAnswers[incorrectFilled];
+                        incorrectFilled++;
+                        answerAdded[random] = true;
+                    }
+                }
+            }
+
+            questionChange = false;
+        }
+    }
+
+    void CheckAnswers()
+    {
+        if(pressurePadReleased && pressurePadPressed != 0)
+        {
+            for(int i = 0; i < correctCount; i++)
+            {
+                if(answers[pressurePadPressed - 1].text == questions[chosenTopic][questionNumber].correctAnswers[i])
+                {
+                    correctAnswers++;
+                }
+            }
+            padCount++;
+            pressurePadPressed = 0;
+
+            if (correctAnswers != correctCount && padCount == correctCount)
+            {
+                questionsAsked++;
+                questionNumber++;
+                ChangeQuestion();
+            }
+            else if (correctAnswers == correctCount && padCount == correctCount)
+            {
+                answeredCorrectly++;
+                questionsAsked++;
+
+                questionNumber++;
+                ChangeQuestion();
+            }
+        }
+    }
+}
---
+++ Assets/Scene/Scripts/PressurePadScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePadScript : MonoBehaviour {
+
+    public GameObject PressurePad;
+    AskQuestion aqScript;
+    GameObject gObj;
+
+
+	// Use this for initialization
+	void Start () {
+        gObj = GameObject.Find("Projector Screen");
+        aqScript = gObj.GetComponent<AskQuestion>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (PressurePad.name == "PressurePadRight")
+        {
+            PressurePad.GetComponent<Animation>().Play("PressurePadDown-R");
+            aqScript.setPressurePad(3);
+        }
+        if (PressurePad.name == "PressurePadLeft")
+        {
+            PressurePad.GetComponent<Animation>().Play("PressurePadDown-L");
+            aqScript.setPressurePad(1);
+        }
+        if (PressurePad.name == "PressurePadFront")
+        {
+            PressurePad.GetComponent<Animation>().Play("PressurePadDown-F");
+            aqScript.setPressurePad(2);
+        }
+        if (PressurePad.name == "PressurePadBack")
+        {
+            PressurePad.GetComponent<Animation>().Play("PressurePadDown-B");
+            aqScript.setPressurePad(4);
+        }
+
+        aqScript.setPressurePadReleased(false);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (PressurePad.name == "PressurePadRight")
+        {
+            PressurePad.GetComponent<Animation>().Play("PressurePadUp-R");
+        }
+        if (PressurePad.name == "PressurePadLeft")
+        {
+            PressurePad.GetComponent<Animation>().Play("PressurePadUp-L");
+        }
+        if (PressurePad.name == "PressurePadFront")
+        {
+            PressurePad.GetComponent<Animation>().Play("PressurePadUp-F");
+        }
+        if (PressurePad.name == "PressurePadBack")
+        {
+            PressurePad.GetComponent<Animation>().Play("PressurePadUp-B");
+        }
+
+        aqScript.setPressurePadReleased(true);
+    }
+}
